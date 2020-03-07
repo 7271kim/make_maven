590988425b388e3937463ded814effea0adc6512
김석진 <7271kim@naver.com>
2020-03-08 00:41:40 +0900

    [2020.03.08] - Resource 세팅 하기

--- src/main/java/com/seokjin/makemaven/Main.java
+++ src/main/java/com/seokjin/makemaven/Main.java
@@ -1,10 +1,21 @@
 package com.seokjin.makemaven;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) throws IOException {
+        boolean isTrue = StringUtils.isNotBlank("aaaaaaaa");
+        String name = "config/config.properties"; 
+        // Resorce 하위 propertiese들 읽기
+        InputStream inputStream = ClassLoader.getSystemClassLoader().getSystemResourceAsStream(name);
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        System.out.println(properties);
     }
 
 }
